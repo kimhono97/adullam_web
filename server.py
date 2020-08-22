f407b2f0421719fc808d5b94ef48af67f76d2999
kimhono97 <kimhono97@gmail.com>
2020-08-22 16:34:51 +0900

    Youtube API 01

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+import yt_api as yt
+import json
 
 app = Flask(__name__)
 
@@ -13,7 +15,34 @@ def page(page):
         return redirect("/")
     return render_template(page + ".html")
 
+@app.route("/sermon", methods=["GET"])
+def sermon():
+    global ytd
+    playlists = ytd.getPlaylists()
+    try:
+        tab = int(request.args.get('t'))
+        if tab < 0 or tab >= len(playlists):
+            tab = 0
+    except:
+        tab = 0
+        for pl in playlists:
+            if "주일" in pl['ptitle']:
+                break
+            tab += 1
+    
+    videos = ytd.getVideos(playlists[tab]['pid'])
+    videos.reverse()
+    tabs = list(enumerate(playlists))
+
+    return render_template("sermon.html", tab=tab, tabs=tabs, videos=videos[:11])
+
+@app.route("/update")
+def update():
+    global ytd
+    ytd.refresh()
+    return "Success!"
 
 if __name__ == "__main__":
-    #app.run(threaded=True, debug=True)
-    app.run(host="172.30.1.25", threaded=True)
+    ytd = yt.YT_Data(yt.key, yt.ch_id['adullam'])
+    app.run(threaded=True, debug=False)
+    #app.run(host="172.30.1.25", threaded=True)
--- yt_api.py
+++ yt_api.py
@@ -2,34 +2,77 @@ import requests
 import json
 
 class YT_Data:
-    def __init__(self):
-        self.key = "AIzaSyDoG6P2w263G7gjtYsi9ryhOhy_NdsLUGQ"
-        self.__ch_id = "UCXZe6SLnxBSB0S6XBaxHK4g"
+    def __init__(self, key, ch_id):
+        self.__key = key
+        self.__ch_id = ch_id
+        self.__url_pref = "https://www.googleapis.com/youtube/v3/"
         self.__playlists = self.__findPlaylists()
+        self.__videos = self.__findVideos()
+        print("<!> YT_Data : Inited")
+        
     def __findPlaylists(self):
-        url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId={0}&key={1}"
+        url = self.__url_pref + "playlists?part=snippet&channelId=%s&key=%s"
         req = requests.get(url % (self.__ch_id, self.__key))
         d = json.loads(req.text)
         out = []
         for pl in d['items']:
             i = {}
-            i['id'] = pl['id']
-            i['title'] = pl['snippet']['title']
-            i['thumbnail'] = pl['snippet']['thumbnails']['default']
+            i['pid'] = pl['id']
+            i['ptitle'] = pl['snippet']['title']
+            i['pthumbnail'] = pl['snippet']['thumbnails']['high']
             out.append(i)
         return out
+    
     def __findVideos(self):
-        url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults={0}&playlistId={1}&key={2}"
+        url = self.__url_pref + "playlistItems?part=snippet&maxResults=%d&playlistId=%s&key=%s"
         out = {}
         for pl in self.__playlists:
-            plid = pl['id']
+            plid = pl['pid']
             out[plid] = []
-            req = requests.get(url % (20, plid, self.__key))
+            req = requests.get(url % (30, plid, self.__key))
+            d_items = []
             d = json.loads(req.text)
-            for vd in d['items']:
-                
+            d_items += d['items']
+            while 'nextPageToken' in d.keys():
+                req = requests.get(url % (30, plid, self.__key) + ('&pageToken=%s' % d['nextPageToken']))
+                d = json.loads(req.text)
+                d_items += d['items']
+            for vd in d_items:
+                i = {}
+                i['vid'] = vd['snippet']['resourceId']['videoId']
+                i['vtitle'] = vd['snippet']['title']
+                i['vdesc'] = vd['snippet']['description']
+                tmp = vd['snippet']['publishedAt']
+                i['vpubat'] = tmp[:10] + " " + tmp[11:-1]
+                i['vthumbnail'] = vd['snippet']['thumbnails']['high']
+                out[plid].append(i)
+        return out
+
     def getPlaylists(self):
         return self.__playlists
 
+    def getVideos(self, pid):
+        return self.__videos[pid]
+    
+    def refresh(self):
+        self.__playlists = self.__findPlaylists()
+        self.__videos = self.__findVideos()
+        print("<!> YT_Data : Refreshed")
+        return
+
+#key = "AIzaSyDoG6P2w263G7gjtYsi9ryhOhy_NdsLUGQ"
+key = "AIzaSyAVGzJe12gm1LzSxPvgYIRQwYQM0Hxjb7I" #temp key
+ch_id = {"adullam" : "UCXZe6SLnxBSB0S6XBaxHK4g"}
+
 if __name__ == "__main__":
-    yt = YT_Data()
+    yt = YT_Data(key, ch_id['adullam'])
+    videos = yt.getVideos()
+    print(json.dumps(videos, indent="\t"))
+
+
+#   youtube.com/watch?v=<vid>
+#   youtube.com/watch?v=<vid>&t=<min>m<sec>s
+#   youtube.com/watch?v=<vid>&list<pid>
+
+#   youtube.com/channel/<ch_id>
+#   youtube.com/playlist?list=<pid>
